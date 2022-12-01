ed5d2cfc5f098f7dc2b86fdb1d807b104a5450ca
Eric Garcia <garciaericsteven@gmail.com>
2022-11-30 16:18:48 -0800

    working local search but very slow
    need to fix cost function

--- main.py
+++ main.py
@@ -1,31 +1,52 @@
 from starter import *
 
-def cost(G: nx.graph, vertex: int, new_team: int, b: np.array = None, b_norm: int = None):
+def main():
+    G = read_input('small1.in')
+    G_naive = read_input('small1.in')
+    solve(G)
+    validate_output(G)
+    calculated_score = score(G)
+    # visualize(G)
+    solve_naive(G_naive)
+    validate_output(G_naive)
+    score_naive = score(G_naive)
+    print("score: ", calculated_score)
+    print("naive score: ", score_naive)
+    print("better by ", score_naive - calculated_score)
+
+def cost(G: nx.graph, vertex: int, new_team: int, b: np.array = None, b_norm: int = None, weight_score: int = None, teams_score: int = None, balance_score: int = None):
+    old_team = G.nodes[vertex]["team"]
+    swap(G, vertex, new_team)
+    new_cost = score(G)
+    swap(G, vertex, old_team)
+    return new_cost
+
+
+
     if b is None or b_norm is None:
         b, b_norm = get_b_and_b_norm(G)
+    if weight_score is None or teams_score is None or balance_score is None:
+        weight_score, teams_score, balance_score = score(G, separated=True)
     old_team = G.nodes[vertex]["team"]
-    print(old_team, new_team)
     b_i = b[old_team - 1]
     b_j = b[new_team - 1]
     V = G.number_of_nodes()
-    new_Cp = math.exp(70 * (b_norm ** 2 - b_i ** 2 - b_j ** 2 + (b_i - 1 / V) ** 2 + (b_j + 1 / V) ** 2) ** (1/2))
-    new_Cw = 0
+    new_balance_score = math.exp(70 * (b_norm ** 2 - b_i ** 2 - b_j ** 2 + (b_i - 1 / V) ** 2 + (b_j + 1 / V) ** 2) ** (1/2))
+    new_weight_score = weight_score
     for neighbor in G.neighbors(vertex):
-        if G.nodes[neighbor]["team"] != new_team:
-            new_Cw += G[vertex][neighbor]["weight"]
-        else:
-            new_Cw -= G[vertex][neighbor]["weight"]
-    return new_Cp + new_Cw
+        if G.nodes[neighbor]["team"] == new_team:
+            new_weight_score -= G[vertex][neighbor]["weight"]
+        if G.nodes[neighbor]["team"] == old_team:
+            new_weight_score += G[vertex][neighbor]["weight"]
+    return new_weight_score + teams_score + new_balance_score
 
 def solve(G: nx.Graph):
-    # Assign a team to v with G.nodes[v]['team'] = team_id
-    # Access the team of v with team_id = G.nodes[v]['team']
     solve_naive(G) # Will be replaced with approximation
-
     local_search(G)
 
 def local_search(G: nx.graph):
     G_intermediate = G
+    teams = list(get_teams_and_counts(G)[0]) # This does not work
     teams = [1, 2]
     unmarked = set(list(G.nodes))
     gains = []
@@ -47,8 +68,12 @@ def local_search(G: nx.graph):
 
     smallest_swap_score = min(gains, key=lambda x: x[2])[2]
 
+    #print(gains)
+    #print(smallest_swap_score)
+
     for v, team, swap_score in gains:
         swap(G, v, team)
+        #print("swapped")
         if swap_score == smallest_swap_score:
             break
     return G
@@ -63,20 +88,6 @@ def solve_naive(G: nx.graph):
     # Partition first half to team 2
     for v in range(num_nodes//2, num_nodes):
         G.nodes[v]['team'] = 2
-
-def main():
-    G = read_input('small1.in')
-    G_naive = read_input('small1.in')
-    solve(G)
-    validate_output(G)
-    calculated_score = score(G)
-    # visualize(G)
-    solve_naive(G_naive)
-    validate_output(G_naive)
-    score_naive = score(G_naive)
-    print("score: ", calculated_score)
-    print("naive score: ", score_naive)
-    print("better by ", calculated_score - score_naive)
     
 def get_teams_and_counts(G: nx.graph):
     output = [G.nodes[v]['team'] for v in range(G.number_of_nodes())]
@@ -93,8 +104,8 @@ def get_b_and_b_norm(G: nx.graph):
  
 def swap(G: nx.graph, v: int, team: int):
     old_team = G.nodes[v]["team"]
-    if old_team == team:
-        print("swapped")
+    #if old_team == team:
+    #    print("swapped")
     G.nodes[v]["team"] = team
     return G
 
